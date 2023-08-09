aa00b56db3a13fda37133b5cfb6aaa060c13302d
Kchun510 <kylechun1@gmail.com>
2023-08-09 13:01:00 -0700

    testing 123

--- src/pages/home.js
+++ src/pages/home.js
@@ -121,6 +121,9 @@ const Home = () => {
       <div className='col'>
       <br></br>
       <button type = "button" className = 'btn btn-secondary' onClick={Build_direct}>Build Your Transfer Plan</button>
+        <Routes>
+          <Route path = '/sort_build'/>
+        </Routes>
       </div>
     </div>
     )
